d4376d21edd3a50d4f95ae5d8d2e004f11f143b3
Rosabelle Armstead <rosabelle.l.armstead@gmail.com>
2017-11-13 16:27:45 +0000

    Create Dog.cpp

---
+++ src/Dog.cpp
@@ -0,0 +1,7 @@
+#include "Dog.h"
+include <string>
+using namespace std;
+
+Dog::Dog(string a_breed, string a_name, string a_colour, string a_earType, string a_height, string a_tailColour, string a_dadsName, string a_mumsName) : Animal(a_breed, a_name, a_colour, a_earType, a_height, a_tailColour, a_dadsName, a_mumsName)
+{
+}
