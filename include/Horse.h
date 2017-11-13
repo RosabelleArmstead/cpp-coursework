e3c98c68bb642f8b075fa605f0e787d4fc4c78eb
Rosabelle Armstead <rosabelle.l.armstead@gmail.com>
2017-11-13 16:25:05 +0000

    Create Horse.h

---
+++ include/Horse.h
@@ -0,0 +1,8 @@
+#include "Animal.h"
+#include <string>
+using namespace std;
+
+class Horse: public Animal{
+public:
+	Animal{string breed, string name, string colour, string earType, string height, string tailColour, string dadsName, string mumsName);
+};
