c64dce83223391a1ecd6ca8c262942d36f279878
Rosabelle Armstead <rosabelle.l.armstead@gmail.com>
2017-11-13 16:28:56 +0000

    Create main.cpp

---
+++ src/main.cpp
@@ -0,0 +1,14 @@
+#include "Dog.h"
+#include "Cat.h"
+#include "Horse.h"
+#include <iostream>
+#include <fstream>
+#include <string>
+using namespace std;
+
+//function declarations - map template? key = name
+void readDogs(){}
+void readCats(){}
+void readHorses(){}
+
+
