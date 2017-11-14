eb135d5bfb23943cfe50dc6a5c9ac3e5e212c8e6
Rosabelle Armstead <rosabelle.l.armstead@gmail.com>
2017-11-14 16:00:30 +0000

    Update main.cpp
    Added skeleton of program with empty functions and comment descriptions

--- src/main.cpp
+++ src/main.cpp
@@ -7,8 +7,34 @@
 using namespace std;
 
 //function declarations - map template? key = name
-void readDogs(){}
-void readCats(){}
-void readHorses(){}
+//read dogs csv into data structure
+void readDogs()
+{
 
+}
+//read cats csv into data structure
+void readCats()
+{
 
+}
+//read horses csv into data structure
+void readHorses()
+{
+
+}
+//produce formatted output
+void display()
+{
+
+}
+//process user input to find paternal family tree
+void processInput()
+{
+
+}
+
+//main method - run read functions, run output function, take input, run input processing function 
+int main()
+{
+	return 0;
+}
