d7f8c5e385ebc633afb09b1f82a2af76480f1193
HPegrum <hp00266@surrey.ac.uk>
2017-12-05 14:11:44 +0000

    Update Animal.cpp

--- src/Animal.cpp
+++ src/Animal.cpp
@@ -2,14 +2,11 @@
 #include <string>
 using namespace std;
 
-Animal::Animal(string a_breed, string a_name, string a_colour, string a_earType, string a_height, string a_tailColour, string a_dadsName, string a_mumsName)
+Animal::Animal(string a_breed, string a_name, Aesthetic a_aesthetics, string a_dadsName, string a_mumsName)
 {
 	breed = a_breed;
 	name = a_name;
-	colour = a_colour;
-	earType = a_earType;
-	height = a_height;
-	tailColour = a_tailColour;
+	aesthetics = a_aesthetics;
 	dadsName = a_dadsName;
 	mumsName = a_mumsName;
 }
@@ -22,20 +19,8 @@ string Animal::getName() {
 	return name;
 }
 
-string Animal::getColour() {
-	return colour;
-}
-
-string Animal::getEarType() {
-	return earType;
-}
-
-string Animal::getHeight() {
-	return height;
-}
-
-string Animal::getTailColour() {
-	return tailColour;
+Aesthetics Animal::getAesthetics() {
+	return aesthetics;
 }
 
 string Animal::getDadsName() {
