d7fe6a9ece24566e0f053e50973805c5e1a6a664
HPegrum <hp00266@surrey.ac.uk>
2017-12-05 14:04:33 +0000

    Create Aesthetics.cpp

---
+++ src/Aesthetics.cpp
@@ -0,0 +1,10 @@
+#include <string>
+using namespace std;
+
+Aesthetics::Aesthetics(string a_colour, string a_earType, string a_height, string a_tailColour)
+{
+colour = a_colour;
+earType = a_earType;
+height = a_height;
+tailColour = a_tailColour;
+}
