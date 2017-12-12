1f6e55ee0f79d90abc365ae1042ebbb28cadad33
Unknown <rosabelle.l.armstead@gmail.com>
2017-12-12 21:15:55 +0000

    Playing around putting code into functions

--- src/main.cpp
+++ src/main.cpp
@@ -9,9 +9,12 @@
 #include <map>
 using namespace std;
 
-map<string, Dog> readDogs(string filename);
-
 //function declarations - map template? key = name
+map<string, Dog> readDogs(string filename);
+string ltrim(string s, const char* t);
+processInput();
+processDog (string inputAnimal, string file);
+chooseFile(string inputAnimal);
 //read dogs csv into data structure
 
 
@@ -54,10 +57,6 @@ map<string, Dog> readDogs(string fileName)
 
 }
 
-
-	
-
-
 //read cats csv into data structure
 void readCats()
 {
@@ -73,38 +72,94 @@ void display()
 {
 
 }
-//process user input to find paternal family tree
-void processInput()
-{
-
+//trim for processing input
+string ltrim(string s, const char* t = " \t\n\r\f\v") {
+	return s.erase(0, s.find_first_not_of(t));
 }
 
+//process user input to find paternal family tree
+int processInput()
+{
+	cout << "Enter the first letter of the animal group and the name of the specified one to find its paternal tree: ";
+	string inputAnimal = "";
+	cin >> inputAnimal;
+	string file = chooseFile(inputAnimal);
+	inputAnimal = ltrim(inputAnimal);
+	
+	if (file == "dogs.csv")
+	{
+		processDog(inputAnimal, file);
+	}
+	else
+	{
+		cout << file << endl;
+	}
+		/* case "cats.csv": processCat(inputAnimal, file);
+		case "horses.csv": processHorse(inputAnimal, file); */
+		//try catch for if animal not existing
+	return 0;
+}
+
+string chooseFile(string inputAnimal)
+{
+	if (inputAnimal.at(1) == "d")
+	{
+		return "dogs.csv";
+	}
+	else if (inputAnimal.at(1) == "c")
+	{
+		return "cats.csv";
+	}
+	else if (inputAnimal.at(1) == "h")
+	{
+		return "horses.csv";
+	}
+	else
+	{
+		return "Not an animal!";
+	}
+}
+
+//methods to process animals - use template and pass classes?
+void processDog (string inputAnimal, string file)
+{
+	map<string, Dog> dogs = readDogs(file);
+	Dog tmp = dogs.at(inputAnimal);
+	cout << "Paternal tree of " << inputAnimal << ":" << endl;
+	cout << inputAnimal << " <-- ";
+	while(dogs.at(tmp.getName()).getDad()!=0) {
+		cout << tmp.getDad()->getName() << " <-- ";
+		tmp = dogs.at(tmp.getDad()->getName());
+	}
+	cout << "[END]" << endl;
+}
+/* void processCat (string inputAnimal, string file)
+{
+	map<string, Cat> cats = readCats(file);
+	Cat tmp cats.at(inputAnimal);
+	cout << "Paternal tree of " << inputAnimal << ":" << endl;
+	cout << inputAnimal << " <-- ";
+	while(cats.at(tmp.getName()).getDad()!=0) {
+		cout << tmp.getDad()->getName() << " <-- ";
+		tmp = cats.at(tmp.getDad()->getName());
+	}
+	cout << "[END]" << endl;
+}
+void processHorse (string inputAnimal, string file)
+{
+	map<string, Horse> horses = readhorses(file);
+	Horse tmp horses.at(inputAnimal);
+	cout << "Paternal tree of " << inputAnimal << ":" << endl;
+	cout << inputAnimal << " <-- ";
+	while(horses.at(tmp.getName()).getDad()!=0) {
+		cout << tmp.getDad()->getName() << " <-- ";
+		tmp = horses.at(tmp.getDad()->getName());
+	}
+	cout << "[END]" << endl;
+}
+*/
 //main method - run read functions, run output function, take input, run input processing function 
 int main()
 {
-	map<string,Dog> dogs = readDogs("dogs.csv");
-		
-	cout << "Enter the first letter of the animal group and the name of the specified one to find its paternal tree: ";
-
-	string inputDogName;
-	cin >> inputDogName;
-
-	Dog tmp = dogs.at(inputDogName);		
-
-	cout << "Paternal tree of " << inputDogName << ":" << endl;
-	cout << inputDogName << " <-- ";
-	while(dogs.at(tmp.getName()).getDad()!=0) {
-
-		cout << tmp.getDad()->getName() << " <-- ";
-		tmp = dogs.at(tmp.getDad()->getName());
-
-	}
-	cout << "[END]" << endl;
-	
-	//cout << dad->getName() << endl;
-
-	
-
-
-	return 0;
+	processInput();
 }
